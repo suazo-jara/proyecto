b489a79a9779af9bc527e6635ede0c51e9cd89bd
UnaCuentaFalsaMas <garahxp@gmail.com>
2021-06-16 17:23:47 -0400

    borrador + crear personaje

--- main.c
+++ main.c
@@ -7,33 +7,32 @@
 #include <windows.h>
 #include <time.h>
 
-
-
 //Estructura
 
 typedef struct
 {
-char Raza[10];
-int Fuerza;       // mide la potencia física y capacidades deportivas.
-int Destreza;     // mide la agilidad y los reflejos.
-int Constitucion; // mide la resistencia y la salud.
-int Inteligencia; // mide el razonamiento y la memoria.
-int Sabiduria;    // mide la perspicacia e intuición.
-int Carisma;      // mide la fuerza de personalidady la presencia
-}Raza;
- 
+   char Raza[10];
+   int Fuerza;       // mide la potencia física y capacidades deportivas.
+   int Destreza;     // mide la agilidad y los reflejos.
+   int Constitucion; // mide la resistencia y la salud.
+   int Inteligencia; // mide el razonamiento y la memoria.
+   int Sabiduria;    // mide la perspicacia e intuición.
+   int Carisma;      // mide la fuerza de personalidady la presencia
+} Raza;
+
 typedef struct
 {
-Raza estadisticas;
-int karma;
-//Se puede agregar mas
-}Personaje;
+   char nombre[30];
+   Raza estadisticas;
+   int karma;
+   //Se puede agregar mas
+} Personaje;
 
 //Variables Globales
 //Para las elccion de Raza
-Raza humano = {"Humano", 1,2,3,4,5} ;
-Raza enano  = {"Enano",  1,2,3,4,5} ;
-Raza Elfo   = {"Elfo",   1,2,3,4,5} ;
+Raza humano = {"Humano", 1, 2, 3, 4, 5};
+Raza enano = {"Enano", 1, 2, 3, 4, 5};
+Raza Elfo = {"Elfo", 1, 2, 3, 4, 5};
 //Por si querremos que navegue con las flechas
 #define arriba 72;
 #define izquierda 75;
@@ -41,18 +40,104 @@ Raza Elfo   = {"Elfo",   1,2,3,4,5} ;
 #define abajo 80;
 #define exit 27;
 
-
 //Pototipos Funciones
+Personaje crearPersonaje();
 int dado();
 
 //Main
-int main(){
-
-
+int main()
+{
+   Personaje jugador = crearPersonaje();
 }
 
 //Funciones
-int dado(){
+int dado()
+{
    srand(time(NULL));
-   int num=rand()%10;
+   int num = rand() % 10;
+}
+
+Personaje crearPersonaje()
+{
+   Personaje usuario;
+   printf("Cual es tu nombre?\n");
+   scanf("%[^\n]s", usuario.nombre);
+   getchar();
+   system("cls");
+   int opcion = 0;
+   while (opcion == 0)
+   {
+      printf("Elije tu Raza\n");
+      printf("Humano(1)  Elfo(2)  Enano(3)\n");
+      scanf("%i", &opcion);
+      system("cls");
+      switch (opcion)
+      {
+      case 1:
+         printf("Descripcion Humano\n");
+         printf("Cancelar(0)/Siguiente(1)\n");
+         scanf("%i", &opcion);
+         system("cls");
+         if (opcion == 1)
+         {
+            usuario.estadisticas = humano;
+         }
+         else
+         {
+            if (opcion != 0)
+            {
+               printf("ingrese una opcion valida");
+               opcion = 0;
+            }
+         }
+         break;
+      case 2:
+         printf("Descripcion Elfo\n");
+         printf("Cancelar(0)/Siguiente(1)\n");
+         scanf("%i", &opcion);
+         system("cls");
+         if (opcion == 1)
+         {
+            usuario.estadisticas = Elfo;
+         }
+         else
+         {
+            if (opcion != 0)
+            {
+               printf("ingrese una opcion valida");
+               opcion = 0;
+            }
+         }
+         break;
+      case 3:
+         printf("Descripcion Enano\n");
+         printf("Cancelar(0)/Siguiente(1)\n");
+         scanf("%i", &opcion);
+         system("cls");
+         if (opcion == 1)
+         {
+            usuario.estadisticas = enano;
+         }
+         else
+         {
+            if (opcion != 0)
+            {
+               printf("ingrese una opcion valida");
+               opcion = 0;
+            }
+         }
+         break;
+
+      default:
+         opcion = 0;
+         break;
+         system("cls");
+      }
+   }
+   FILE *archivo = fopen("personaje.csv", "w");
+
+   fprintf(archivo, "%s,%s,%i,%i,%i,%i,%i,%i", usuario.nombre, usuario.estadisticas.Raza, usuario.estadisticas.Fuerza, usuario.estadisticas.Destreza,
+           usuario.estadisticas.Constitucion, usuario.estadisticas.Inteligencia, usuario.estadisticas.Sabiduria, usuario.estadisticas.Carisma, usuario.karma);
+   fclose(archivo);
+   return usuario;
 }
