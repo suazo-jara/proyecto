38066324505e6936b2c6a042be03af246cbe1d74
kevin-meza-toro <80593263+kevin-meza-toro@users.noreply.github.com>
2021-06-26 19:08:37 -0400

    Avanzar función juego y varias funciones

--- main.c
+++ main.c
@@ -36,10 +36,14 @@ typedef struct{
 void leer_archivo(Map*);
 void pasar_consecuencia(char *,NPC*, int);
 void display_menu();
-void juego(Map *);
+void juego(Map *,int*,int*,int, nacion*);
+int dado_personajes(Map *, int *,int);
 
 //Funciones auxiliares
-void leer_tecla();
+void mostrar_nacion(nacion*);
+void mostrar_consecuencias(NPC*, int);
+void cambiar_nacion(NPC *, int, nacion*);
+int leer_tecla();
 void mostrar_personajes(Map *);
 int is_equal_int(void *, void *);
 int is_equal_float(void *, void *);
@@ -51,10 +55,21 @@ int lectura;
 int main(){
    Map *personajes = createMap(is_equal_int);
    setSortFunction(personajes,lower_than_int);
-   
+
+   nacion *reino = (nacion*)malloc(sizeof(nacion));
+    reino->religion = 10;
+    reino->corrupcion = 10;
+    reino->economia = 10;
+    reino->felicidad = 10;
+    reino->defensa = 10;
+
+   int cont = 0;
+   int cantidad = 2;
+   int array_personaje[2] ={};
+   int array_eventos[2] = {};
 
    leer_archivo(personajes);
-   mostrar_personajes(personajes);
+   //mostrar_personajes(personajes);
 
    printf("\nEl anterior lord regente del reino de La Embarrada acaba de huir de sus labores.\n");
    printf("Cuando pasabas cerca del castillo, te entrega toda su autoridad.\n");
@@ -65,17 +80,30 @@ int main(){
    }
    system("cls");//limpia terminal
    lectura=3;
+   display_menu();
    while(lectura != 0){
-       display_menu();
        leer_tecla();
        if (lectura == 1){
-           juego(personajes);
+           while(cont<cantidad){
+               //Mostrar cualidades nacion
+               mostrar_nacion(reino);
+               //Juego
+               juego(personajes,array_personaje,array_eventos,cantidad, reino);
+               cont++; 
+               system("cls"); 
+           }
+           if(cont == cantidad){
+               break;
+           }
        }
     }
-   
-   
-   printf("Huyes despavorido de tus labores.\n\n");
-   printf("(Presiona cualquier tecla para salir...)");
+   if(cont == cantidad){
+       printf("FELICIDADES superaste todos los niveles\n"); 
+   }else{
+       printf("Huyes despavorido de tus labores.\n\n");
+       printf("(Presiona cualquier tecla para salir...)");
+   }
+
    while(!kbhit()){
    }
    system("cls");
@@ -170,13 +198,60 @@ void display_menu(){
     printf("Escriba la opcion que desee: ");
 }
 
-void juego(Map * personajes){
-    nacion *reino = (nacion*)malloc(sizeof(nacion));
-    reino->religion = 10;
-    reino->corrupcion = 10;
-    reino->economia = 10;
-    reino->felicidad = 10;
-    reino->religion = 10;
+void juego(Map * personajes, int *array_personajes, int *array_eventos, int cantidad, nacion *reino){
+
+    NPC *iterador;
+    int numero;
+    int cont = 0;
+    int lectura = 3;
+    int opcion = 0;
+
+    //Lanzar dado para seleccionar personaje
+    numero = dado_personajes(personajes, array_personajes, cantidad);
+    //Buscar personaje
+    iterador = searchMap(personajes,&numero);
+
+    //Mostrar personaje
+    printf("Mi nombre es %s, te tengo la siguiente propuesta:\n", iterador->nombre);
+    printf("%s\n", iterador->peticion);
+    //Opcion 0
+    printf("Elige una de las dos opciones!!\n\n");
+    printf("0. %s \n", iterador->opcion_a);
+    mostrar_consecuencias(iterador, opcion);
+    //Opcion 1
+    opcion = 1;
+    printf("1. %s \n", iterador->opcion_b);
+    mostrar_consecuencias(iterador, opcion);
+    
+    //Seleccionar opcion
+    printf("Elija una opcion");
+    lectura = leer_tecla();
+    cambiar_nacion(iterador, lectura, reino);
+    
+
+}
+
+void mostrar_nacion(nacion *reino){
+    printf("--Cualidades nacion--\n");
+    printf("-Corrupcion: %i \n", reino->corrupcion);
+    printf("-Defensa: %i \n", reino->defensa);
+    printf("-Economia: %i \n", reino->economia);
+    printf("-Felicidad: %i \n", reino->felicidad);
+    printf("-Religion: %i \n\n", reino->religion);
+}
+
+int dado_personajes(Map *personajes, int *array_personajes, int cantidad){
+    srand(time(NULL));
+    int verificar = 0;
+    int numero;
+    while(verificar == 0){
+        numero = rand() % cantidad;
+        if(array_personajes[numero] == 0){
+            array_personajes[numero] = 1;
+            verificar = 1;
+            return numero;
+        }
+    }
 }
 
 void mostrar_personajes(Map *personajes){
@@ -223,7 +298,100 @@ void mostrar_personajes(Map *personajes){
     }
 }
 
-void leer_tecla()
+void mostrar_consecuencias(NPC *iterador, int opcion){
+
+    if(opcion == 0){
+        printf("CONSECUENCIAS: | ");
+        for(int i = 0; i<5;i++){
+            if(iterador->consecuencia_a[i] != 0){
+                if(i == 0) printf("Economia: ");
+                if(i == 1) printf("Religion: ");
+                if(i == 2) printf("Defensa: ");
+                if(i == 3) printf("Felicidad: ");
+                if(i == 4) printf("Corrupcion: ");
+                if(iterador->consecuencia_a[i] > 0) printf("+");
+                printf("%i | ", iterador->consecuencia_a[i]);
+            }
+        }
+        printf("\n");
+        printf("\n");
+    }
+    if(opcion == 1){
+        printf("CONSECUENCIAS: | ");
+        for(int i = 0; i<5;i++){
+            if(iterador->consecuencia_a[i] != 0){
+                if(i == 0) printf("Economia: ");
+                if(i == 1) printf("Religion: ");
+                if(i == 2) printf("Defensa: ");
+                if(i == 3) printf("Felicidad: ");
+                if(i == 4) printf("Corrupcion: ");
+                if(iterador->consecuencia_b[i] > 0) printf("+");
+                printf("%i | ", iterador->consecuencia_b[i]);
+            }
+        }
+        printf("\n");
+        printf("\n");
+    }
+}
+
+void cambiar_nacion(NPC *iterador, int lectura, nacion *reino){
+    int cambio;
+
+    if(lectura == 0){
+        for(int i = 0; i<5;i++){
+            if(iterador->consecuencia_a[i] != 0){
+                if(i == 0){
+                    cambio = iterador->consecuencia_a[i];
+                    reino->economia += cambio;
+                }
+                if(i == 1){
+                    cambio = iterador->consecuencia_a[i];
+                    reino->religion += cambio;
+                }
+                if(i == 2){
+                    cambio = iterador->consecuencia_a[i];
+                    reino->defensa += cambio;
+                }
+                if(i == 3){
+                    cambio = iterador->consecuencia_a[i];
+                    reino->felicidad += cambio;
+                }
+                if(i == 4){
+                    cambio = iterador->consecuencia_a[i];
+                    reino->corrupcion += cambio;
+                }
+            }
+        }
+    }
+    if(lectura == 1){
+        for(int i = 0; i<5;i++){
+            if(iterador->consecuencia_a[i] != 0){
+                if(i == 0){
+                    cambio = iterador->consecuencia_b[i];
+                    reino->economia += cambio;
+                }
+                if(i == 1){
+                    cambio = iterador->consecuencia_b[i];
+                    reino->religion += cambio;
+                }
+                if(i == 2){
+                    cambio = iterador->consecuencia_b[i];
+                    reino->defensa += cambio;
+                }
+                if(i == 3){
+                    cambio = iterador->consecuencia_b[i];
+                    reino->felicidad += cambio;
+                }
+                if(i == 4){
+                    cambio = iterador->consecuencia_b[i];
+                    reino->corrupcion += cambio;
+                }
+            }
+        }
+    }
+}
+
+int leer_tecla()
 {
     int valido = 0;
     char letra;
@@ -276,6 +444,7 @@ void leer_tecla()
         }
     }
     system("cls");
+    return lectura;
 }
 
 //Función para comparar claves de tipo int. Retorna 1 si son iguales
